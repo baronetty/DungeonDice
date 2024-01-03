c55fc3d73021992997c67bf2abe72117edc519a7
Leo Barnett <leo.e.barnett@icloud.com>
2024-01-03 18:37:10 +0200

    Version 1.0

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -23,37 +23,86 @@ struct ContentView: View {
     }
     
     @State private var resultMessage = ""
+    @State private var buttonsLeftOver = 0 // # of buttons in a less-than-full row
+    
+    let horizontalPadding: CGFloat = 16
+    let spacing: CGFloat = 0 // between buttons
+    let buttonWidth: CGFloat = 102
     
     var body: some View {
-        VStack {
-            
-            Text("Dungeon Dice")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundStyle(.red)
-            
-            Spacer()
-            
-            Text(resultMessage)
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .multilineTextAlignment(.center)
-                .frame(height: 150)
-            
-            Spacer()
-            
-            
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
-                ForEach(Dice.allCases, id: \.self) { dice in
-                    Button("\(dice.rawValue)-sided") {
-                        resultMessage = "You roled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+        GeometryReader { geo in
+            VStack {
+                
+                titleView
+                
+                Spacer()
+                
+                resultMessageView
+                
+                Spacer()
+                
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)]) {
+                    ForEach(Dice.allCases.dropLast(buttonsLeftOver), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You roled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                        }
+                        .frame(width: buttonWidth)
                     }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.red)
+                
+                HStack {
+                    ForEach(Dice.allCases.suffix(buttonsLeftOver), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You roled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                        }
+                        .frame(width: buttonWidth)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
+            }
+            .padding()
+            .onChange(of: geo.size.width) { oldValue, newValue in
+                arrangeGridItems(deviceWidth: geo.size.width)
+            }
+            .onAppear {
+                arrangeGridItems(deviceWidth: geo.size.width)
             }
         }
-        .padding()
+    }
+    
+    func arrangeGridItems(deviceWidth: CGFloat) {
+        var screenWidth = deviceWidth - horizontalPadding * 2 // padding on both sides
+        
+        if Dice.allCases.count > 1 {
+            screenWidth += spacing
+        }
+        
+        // calculate the numOfButtonsPerRow as an Int
+        let numberOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+        buttonsLeftOver = Dice.allCases.count % numberOfButtonsPerRow
+        print("numberOfButtonsPerRow = \(numberOfButtonsPerRow)")
+        print("buttonsLeftOver = \(buttonsLeftOver)")
+    }
+}
+
+extension ContentView {
+    private var titleView: some View {
+        Text("Dungeon Dice")
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundStyle(.red)
+    }
+    
+    private var resultMessageView: some View {
+        Text(resultMessage)
+            .font(.largeTitle)
+            .fontWeight(.medium)
+            .multilineTextAlignment(.center)
+            .frame(height: 150)
     }
 }
 
